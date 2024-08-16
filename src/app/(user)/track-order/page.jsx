e25ad09b6961db8c4bc6a2873311e9491c2775dd
Kaushikumar Parmar <kaushik.parmar@geokeyaccess.com>
2024-08-16 21:04:24 +0530

    Fix

--- src/app/(user)/track-order/page.jsx
+++ src/app/(user)/track-order/page.jsx
@@ -29,7 +29,7 @@ export default function Page() {
           }
         ]}
       />
-      <Container maxWidth="xl"> 
+      <Container maxWidth="xl">
         <Typography sx={{ mb: 5, textAlign: 'center' }}>
           To track your order please enter your Order ID in the box below and press the "Track" button. This was given
           to you on your receipt and in the confirmation email you should have received.
