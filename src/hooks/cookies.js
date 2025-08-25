b9d79f00ca0c6f67fda7332341c6314b335ac384
Kaushikumar Parmar <parmarkaushik15@gmail.com>
2025-08-25 11:31:15 +0530

    FIx

--- src/hooks/cookies.js
+++ src/hooks/cookies.js
@@ -7,9 +7,9 @@ export async function createCookies(name, token) {
 
   // Set the cookie with a maxAge of 1 day from now
   const OneDay = 24 * 60 * 60 * 1000;
-  cookies().set(name, token, { maxAge: OneDay });
+  await cookies().set(name, token, { maxAge: OneDay });
 }
 
 export async function deleteCookies(name) {
-  cookies().delete(name);
+  await cookies().delete(name);
 }
